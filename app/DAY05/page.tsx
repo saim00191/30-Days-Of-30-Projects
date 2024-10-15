57d17a0464fab8b809eb05960328a579d83f8db1
saim00191 <saimamjad198@gmail.com>
2024-10-15 23:06:06 +0500

    update

---
+++ app/DAY05/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import CalculatorApp from './DAY05-Components/Calculator'
+
+const Home = () => {
+  return (
+      <div>
+          <CalculatorApp/>
+    </div>
+  )
+}
+
+export default Home
---
+++ app/DAY06/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import DigitalClock from './DAY06-Components/digitalClock'
+
+const Home = () => {
+  return (
+      <div>
+          <DigitalClock/>
+    </div>
+  )
+}
+
+export default Home
