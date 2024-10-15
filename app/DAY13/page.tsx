32b1dec3085de87298941021ff3f7fcec452587d
saim00191 <saimamjad198@gmail.com>
2024-10-15 23:24:21 +0500

    update

---
+++ app/DAY13/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import HTMLPreviewApp from './DAY13-Components/html-preview-app'
+
+const Home = () => {
+  return (
+      <div>
+        <HTMLPreviewApp/>
+    </div>
+  )
+}
+
+export default Home
---
+++ app/DAY14/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import PomodoroTimer from './DAY14-Components/pomodoro-timer-app'
+
+const Home = () => {
+  return (
+      <div>
+          <PomodoroTimer/>
+    </div>
+  )
+}
+
+export default Home
