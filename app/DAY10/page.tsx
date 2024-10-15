960f51b6dbca2efe9f45e125436b3f5f4156faca
saim00191 <saimamjad198@gmail.com>
2024-10-15 23:15:31 +0500

    update

---
+++ app/DAY10/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import PasswordGenerator from './DAY10-Components/passwrod-generator'
+
+const Home = () => {
+  return (
+      <div>
+          <PasswordGenerator/>
+    </div>
+  )
+}
+
+export default Home
---
+++ app/DAY11/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import BMICalculator from './DAY11-Components/bmi-calculator'
+
+const Home = () => {
+  return (
+      <div>
+          <BMICalculator/>
+    </div>
+  )
+}
+
+export default Home
