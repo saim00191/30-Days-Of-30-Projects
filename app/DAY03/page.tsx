53042dd21321ceb33a1d2c2c7cfce3924a372276
saim00191 <saimamjad198@gmail.com>
2024-10-15 22:44:58 +0500

    update

---
+++ app/DAY03/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BirthdayWishApp from './DAY03-Components/birthday-wish-app'
+
+const Home = () => {
+  return (
+      <div>
+          <BirthdayWishApp/>
+    </div>
+  )
+}
+
+export default Home
