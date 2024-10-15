4996df5408080f5d7ce35c109189ae8ef5aa5a72
saim00191 <saimamjad198@gmail.com>
2024-10-15 23:02:11 +0500

    update

---
+++ app/DAY17/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-slate-600 min-h-screen flex justify-center items-center">
+      <h1 className="text-4xl font-bold text-white">
+        The developer has not yet uploaded the specified Day.
+      </h1>
+    </div>
+  );
+}
---
+++ app/DAY18/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import CurrencyConverter from './DAY18-Components/Currency-Converter'
+
+const Home = () => {
+  return (
+      <div>
+          <CurrencyConverter/>
+    </div>
+  )
+}
+
+export default Home
