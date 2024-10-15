44c846384da543ac43fca4427a08a51daa2a543c
saim00191 <saimamjad198@gmail.com>
2024-10-15 23:12:37 +0500

    update

---
+++ app/DAY08/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import ColorPicker from './DAY08-Components/color-picker-app'
+
+const Home = () => {
+  return (
+      <div>
+          <ColorPicker/>
+    </div>
+  )
+}
+
+export default Home
---
+++ app/DAY09/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import TipCalculator from './DAY09-Components/tip-calculator'
+
+const Home = () => {
+  return (
+      <div>
+          <TipCalculator/>
+    </div>
+  )
+}
+
+export default Home
