f386e5f667b6d8d1d9d3d0a5d7a5381e9b974062
saim00191 <saimamjad198@gmail.com>
2024-10-15 22:58:45 +0500

    update

---
+++ app/DAY16/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import MovieSearchApp from './DAY16-Components/movie-search-app'
+
+const Home = () => {
+  return (
+      <div>
+          <MovieSearchApp/>
+    </div>
+  )
+}
+
+export default Home
